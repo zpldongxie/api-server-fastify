ffcfdd210678b44a0ea9e11634d3fd1c45deac41
zhupengliang <zpl_dongxie@tom.com>
2020-10-16 20:26:27 +0800

    优化rest接口代码，提高可读性

--- src/routes/rest/index.js
+++ src/routes/rest/index.js
@@ -2,7 +2,7 @@
  * @description rest接口，不做身份验证，其他系统使用的路由要加验证
  * @author: zpl
  * @Date: 2020-07-30 11:26:02
- * @LastEditTime: 2020-10-13 11:08:04
+ * @LastEditTime: 2020-10-16 14:13:45
  * @LastEditors: zpl
  */
 const fp = require('fastify-plugin');
@@ -15,23 +15,189 @@ module.exports = fp(async (server, opts, next) => {
   const articleMethod = new CommonMethod(mysqlModel.Article);
   const { ajv } = opts;
 
+  /**
+   * 获取菜单
+   *
+   * @param {*} req
+   * @param {*} reply
+   */
+  const getMenu = async (req, reply) => {
+    const runFun = async () => {
+      channelMethod.findAll(
+          reply,
+          {
+            order: [['orderIndex', 'DESC']],
+          },
+      );
+    };
+
+    // 统一捕获异常
+    commonCatch(runFun, reply)();
+  };
+
+  /**
+   * 按条件获取发布的文章列表
+   *
+   * @param {*} req
+   * @param {*} reply
+   * @return {*}
+   */
+  const getPubList = async (req, reply) => {
+    const validate = ajv.compile(querySchema.body.valueOf());
+    const valid = validate(req.body);
+    if (!valid) {
+      return reply.code(400).send(validate.errors);
+    }
+
+    const runFun = async () => {
+      const {
+        channelId,
+        current = 1,
+        pageSize = 20,
+        orderName,
+        orderValue,
+      } = req.body;
+      const attributes = {
+        exclude: ['mainCon'],
+      };
+      const sorter = {
+        'orderIndex': 'desc',
+        'conDate': 'desc',
+      };
+      if (orderName) {
+        sorter[orderName] = (orderValue && orderValue.toLowerCase() === 'desc') ? 'desc' : 'asc';
+      }
+      const include = {
+        model: mysqlModel.Channel,
+        attributes: ['id', 'name', 'enName'],
+        where: {
+          id: channelId,
+        },
+      };
+      articleMethod.queryList(
+          reply,
+          {
+            'pubStatus': '已发布',
+          },
+          current, pageSize, sorter, null, include, attributes,
+      );
+    };
+
+    // 统一捕获异常
+    commonCatch(runFun, reply)();
+  };
+
+  /**
+   * 获取头条文章列表
+   *
+   * @param {*} req
+   * @param {*} reply
+   */
+  const getHeadList = async (req, reply) => {
+    const runFun = async () => {
+      const where = {
+        'pubStatus': '已发布',
+        'isHead': true,
+      };
+      const sorter = {
+        'orderIndex': 'desc',
+        'conDate': 'desc',
+      };
+      const attributes = {
+        exclude: ['mainCon'],
+      };
+      const include = {
+        model: mysqlModel.Channel,
+        attributes: ['id', 'name', 'enName'],
+      };
+      articleMethod.queryList(reply, where, 1, 6, sorter, null, include, attributes);
+    };
+
+    // 统一捕获异常
+    commonCatch(runFun, reply)();
+  };
+
+  /**
+   * 获取推荐文章列表
+   *
+   * @param {*} req
+   * @param {*} reply
+   */
+  const getRecomList = async (req, reply) => {
+    const runFun = async () => {
+      const where = {
+        'pubStatus': '已发布',
+        'isRecom': true,
+      };
+      const sorter = {
+        'orderIndex': 'desc',
+        'conDate': 'desc',
+      };
+      const attributes = {
+        exclude: ['mainCon'],
+      };
+      const include = {
+        model: mysqlModel.Channel,
+        attributes: ['id', 'name', 'enName'],
+      };
+      articleMethod.queryList(reply, where, 1, 6, sorter, null, include, attributes);
+    };
+
+    // 统一捕获异常
+    commonCatch(runFun, reply)();
+  };
+
+  /**
+   * 根据ID获取文章信息
+   *
+   * @param {*} req
+   * @param {*} reply
+   */
+  const getContent = async (req, reply) => {
+    const runFun = async () => {
+      const id = req.params.id;
+      const include = {
+        model: mysqlModel.Channel,
+        attributes: ['id', 'name'],
+      };
+      articleMethod.findOne(reply, id, include);
+    };
+
+    // 统一捕获异常
+    commonCatch(runFun, reply)();
+  };
+
+  /*
+  *                        _oo0oo_
+  *                       o8888888o
+  *                       88" . "88
+  *                       (| -_- |)
+  *                       0\  =  /0
+  *                     ___/`---'\___
+  *                   .' \\|     |// '.
+  *                  / \\|||  :  |||// \
+  *                 / _||||| -:- |||||- \
+  *                |   | \\\  - /// |   |
+  *                | \_|  ''\---/''  |_/ |
+  *                \  .-\__  '-'  ___/-. /
+  *              ___'. .'  /--.--\  `. .'___
+  *           ."" '<  `.___\_<|>_/___.' >' "".
+  *          | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+  *          \  \ `_.   \_ __\ /__ _/   .-` /  /
+  *      =====`-.____`.___ \_____/___.-`___.-'=====
+  *                        `=---='
+  *
+  *
+  *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  *
+  *            佛祖保佑       永不宕机     永无BUG
+  */
+
+  // 获取菜单
   server.get(
       '/rest/menu',
-      {
-        schema: { tags: ['rest'], summary: '获取导航菜单' },
-      }, async (req, reply) => {
-        const runFun = async () => {
-          channelMethod.findAll(
-              reply,
-              {
-                order: [['orderIndex', 'DESC']],
-              },
-          );
-        };
-
-        // 统一捕获异常
-        commonCatch(runFun, reply)();
-      },
+      { schema: { tags: ['rest'], summary: '获取导航菜单' } },
+      getMenu,
   );
 
   // 按条件获取发布的文章列表
@@ -39,179 +205,30 @@ module.exports = fp(async (server, opts, next) => {
   server.post(
       '/rest/getPubList',
       { schema: { ...querySchema, tags: ['rest'], summary: '按条件获取发布的文章列表' } },
-      async (req, reply) => {
-        const validate = ajv.compile(querySchema.body.valueOf());
-        const valid = validate(req.body);
-        if (!valid) {
-          return reply.code(400).send(validate.errors);
-        }
-
-        const runFun = async () => {
-          const {
-            channelId,
-            current = 1,
-            pageSize = 20,
-            orderName,
-            orderValue,
-          } = req.body;
-          const attributes = {
-            exclude: ['mainCon'],
-          };
-          const sorter = {
-            'orderIndex': 'desc',
-            'conDate': 'desc',
-          };
-          if (orderName) {
-            sorter[orderName] = (orderValue && orderValue.toLowerCase() === 'desc') ? 'desc' : 'asc';
-          }
-          const include = {
-            model: mysqlModel.Channel,
-            attributes: ['id', 'name', 'enName'],
-            where: {
-              id: channelId,
-            },
-          };
-          articleMethod.queryList(
-              reply,
-              {
-                'pubStatus': '已发布',
-              },
-              current, pageSize, sorter, null, include, attributes,
-          );
-        };
-
-        // 统一捕获异常
-        commonCatch(runFun, reply)();
-      },
+      getPubList,
   );
 
   // 获取头条文章列表
   server.get(
       '/rest/headList',
       { schema: { tags: ['rest'], summary: '获取头条文章列表' } },
-      async (req, reply) => {
-        const runFun = async () => {
-          const where = {
-            'pubStatus': '已发布',
-            'isHead': true,
-          };
-          const sorter = {
-            'orderIndex': 'desc',
-            'conDate': 'desc',
-          };
-          const attributes = {
-            exclude: ['mainCon'],
-          };
-          const include = {
-            model: mysqlModel.Channel,
-            attributes: ['id', 'name', 'enName'],
-          };
-          articleMethod.queryList(reply, where, 1, 6, sorter, null, include, attributes);
-        };
-
-        // 统一捕获异常
-        commonCatch(runFun, reply)();
-      },
+      getHeadList,
   );
 
   // 获取推荐文章列表
   server.get(
       '/rest/recomList',
       { schema: { tags: ['rest'], summary: '获取推荐文章列表' } },
-      async (req, reply) => {
-        const runFun = async () => {
-          const where = {
-            'pubStatus': '已发布',
-            'isRecom': true,
-          };
-          const sorter = {
-            'orderIndex': 'desc',
-            'conDate': 'desc',
-          };
-          const attributes = {
-            exclude: ['mainCon'],
-          };
-          const include = {
-            model: mysqlModel.Channel,
-            attributes: ['id', 'name', 'enName'],
-          };
-          articleMethod.queryList(reply, where, 1, 6, sorter, null, include, attributes);
-        };
-
-        // 统一捕获异常
-        commonCatch(runFun, reply)();
-      },
+      getRecomList,
   );
-
-  // // 获取单片文章
-  // server.get('/channelSingleContent/:id', {}, async (req, reply) => {
-  //   try {
-  //     const id = req.params.id;
-  //     const channel = await mysqlModel.Channel.findOne({
-  //       where: {
-  //         id,
-  //         channelType: '单篇文章',
-  //       },
-  //       include: {
-  //         model: mysqlModel.ContentDetail,
-  //         where: {
-  //           pubStatus: '已发布',
-  //         },
-  //       },
-  //     });
-  //     if (channel) {
-  //       const content = channel.ContentDetails.length ? channel.ContentDetails[0] : {};
-  //       reply.code(200).send(content);
-  //     } else {
-  //       reply.code(404);
-  //     }
-  //   } catch (error) {
-  //     return onRouteError(error, reply);
-  //   }
-  // });
 
   // 按ID获取文章内容
   const getByIdSchema = require('./query-content-by-id-schema');
   server.get(
       '/rest/content/:id',
       { schema: { ...getByIdSchema, tags: ['rest'], summary: '按ID获取文章内容' } },
-      async (req, reply) => {
-        const runFun = async () => {
-          const id = req.params.id;
-          const include = {
-            model: mysqlModel.Channel,
-            attributes: ['id', 'name'],
-          };
-          articleMethod.findOne(reply, id, include);
-        };
-
-        // 统一捕获异常
-        commonCatch(runFun, reply)();
-      },
+      getContent,
   );
-
-  // // 培训报名
-  // const putTrainingRegSchema = require('./put-training-reg-schema');
-  // server.put('/trainingReg', { schema: putTrainingRegSchema }, async (request, reply) => {
-  //   const validate = ajv.compile(putTrainingRegSchema.body.valueOf());
-  //   const valid = validate(request.body);
-  //   if (!valid) {
-  //     return reply.code(400).send(validate.errors);
-  //   }
-
-  //   const trainingResult = await findOne(mysqlModel.Training)({ id: request.body.TrainingId });
-  //   if (trainingResult.status) {
-  //     const result = await create(mysqlModel.TrainingReg)(request.body);
-  //     if (result.status) {
-  //       return reply.code(201).send(result);
-  //     }
-  //     return reply.code(422).send(result);
-  //   }
-  //   return reply.code(422).send({
-  //     status: 'error',
-  //     message: '指定培训不存在，无法创建报名。',
-  //   });
-  // });
 
   next();
 });
