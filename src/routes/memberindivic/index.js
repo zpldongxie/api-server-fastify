c87ef063fca3c2c8ac0086970b4e57294c840bdb
zhupengliang <zpl_dongxie@sina.com>
2021-01-12 18:39:23 +0800

    增加个人会员审核接口

--- src/routes/memberindivic/index.js
+++ src/routes/memberindivic/index.js
@@ -2,11 +2,12 @@
  * @description: 路由
  * @author: zpl
  * @Date: 2020-08-02 13:19:12
- * @LastEditTime: 2021-01-11 16:47:29
+ * @LastEditTime: 2021-01-12 09:09:49
  * @LastEditors: zpl
  */
 const fp = require('fastify-plugin');
 const { Op } = require('sequelize');
+const { memberStatus } = require('../../dictionary');
 const { commonCatch, CommonMethod, onRouteError } = require('../util');
 
 const routerBaseInfo = {
@@ -17,6 +18,7 @@ const routerBaseInfo = {
   getListURL: '/api/getMemberIndivicList',
   putURL: '/api/memberindivic',
   deleteURL: '/api/memberindivics',
+  auditURL: '/api/memberindivic/audit',
 };
 module.exports = fp(async (server, opts, next) => {
   const mysqlModel = server.mysql.models;
@@ -109,26 +111,68 @@ module.exports = fp(async (server, opts, next) => {
           if (id) {
             // 编辑
             const idNumberRes = await routerMethod.dao.findAll({
-              where: { id: { [Op.not]: id }, idNumber },
+              where: {
+                id: { [Op.not]: id },
+                idNumber, status: { [Op.not]: memberStatus.reject },
+              },
             });
             if (idNumberRes.status && idNumberRes.data.length) {
               return onRouteError(reply, { status: 200, message: '证件号已经注册或正在申请' });
             }
             const mobileRes = await routerMethod.dao.findAll({
-              where: { id: { [Op.not]: id }, mobile },
+              where: {
+                id: { [Op.not]: id },
+                mobile, status: { [Op.not]: memberStatus.reject },
+              },
             });
             if (mobileRes.status && mobileRes.data.length) {
               return onRouteError(reply, { status: 200, message: '手机号已经注册或正在申请' });
             }
             await routerMethod.updateOne(reply, id, request.body);
           } else {
-            const res = await routerMethod.dao.findAll({ where: { [Op.or]: [{ idNumber }, { mobile }] } });
+            const res = await routerMethod.dao.findAll({
+              where: {
+                status: { [Op.not]: memberStatus.reject },
+                [Op.or]: [{ idNumber }, { mobile }],
+              },
+            });
             if (res.status && res.data.length) {
               return onRouteError(reply, { status: 200, message: '证件号或手机号已经提交过申请，请不要重复提交' });
             }
             // 新增
             await routerMethod.create(reply, request.body);
           }
+        };
+
+        // 统一捕获异常
+        commonCatch(runFun, reply)();
+      },
+  );
+
+  // 审核
+  const auditSchema = require('./audit-schema');
+  server.post(routerBaseInfo.auditURL,
+      { schema: { ...auditSchema, tags: ['memberindivic'], summary: '审核' } },
+      async (request, reply) => {
+      // 参数校验
+        const validate = ajv.compile(auditSchema.body.valueOf());
+        const valid = validate(request.body);
+        if (!valid) {
+          return reply.code(400).send(validate.errors);
+        }
+
+        // 执行方法
+        const runFun = async () => {
+          const { id, status } = request.body;
+          const res = await routerMethod.dao.findOne({ id });
+          if (!res.status) {
+            return onRouteError(reply, { status: 200, message: 'ID错误，请确认后重新提交' });
+          }
+          let logonDate = null;
+          if (res.data.status !== memberStatus.formalMember && status === memberStatus.formalMember) {
+            logonDate = getCurrentDate();
+          }
+          await routerMethod.updateOne(reply, id, { ...request.body, logonDate });
         };
 
         // 统一捕获异常
