860717c82d10fdadb9e92ae4c58e9d3405cc6703
zhupengliang <zpl_dongxie@sina.com>
2021-03-13 18:35:21 +0800

    所有查询接口增加过滤参数

--- src/routes/article/query-list-schema.js
+++ src/routes/article/query-list-schema.js
@@ -4,6 +4,7 @@ const bodyJsonSchema = S.object()
     .prop('pageSize', S.number().default(10).description('分页大小，默认为10'))
     .prop('current', S.number().default(1).description('当前页码，默认为1'))
     .prop('search', S.string().description('模糊查询条件'))
+    .prop('filter', S.object().description('筛选条件'))
     .prop('sorter', S.object().description('排序条件'))
     .required([]);
 
