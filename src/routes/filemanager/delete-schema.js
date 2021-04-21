9ffce1ad377ae2eb7de9321683b842b1ca02f6d6
zhupengliang <zpl_dongxie@sina.com>
2021-04-21 13:53:00 +0800

    删除旧代码

--- src/routes/filemanager/delete-schema.js
+++
@@ -1,9 +0,0 @@
-const S = require('fluent-schema');
-
-const bodyJsonSchema = S.object()
-    .prop('filePath', S.string().description('文件相对路径'))
-    .prop('fileName', S.string().description('文件名'));
-
-module.exports = {
-  body: bodyJsonSchema,
-};
